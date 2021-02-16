9fa559f2c0fb7cba411485ea61b906e2b5c29d7b
rajchunara <rajchunara777@gmail.com>
2021-02-16 11:22:29 -0500

    first commit to connect to remote repositories

---
+++ src/main/java/EclipseGit/VersionControlling/App.java
@@ -0,0 +1,13 @@
+package EclipseGit.VersionControlling;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
